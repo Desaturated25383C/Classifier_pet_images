652bca3d0e1fea1c294ece99475f343dedc153dd
Wei Zhou <77592507+Desaturated25383C@users.noreply.github.com>
2021-02-05 15:04:56 +0000

    Update calculates_results_stats.py

--- calculates_results_stats.py
+++ calculates_results_stats.py
@@ -116,6 +116,13 @@ def calculates_results_stats(results_dic):
     # Calculates % correct for matches
     results_stats_dic['pct_match'] = ((results_stats_dic['n_match'] / 
                                       results_stats_dic['n_images']) * 100.0)
+    # handle zero division
+    if results_stats_dic['n_dogs_img'] > 0:
+        results_stats_dic['pct_correct_breed'] = 100.0* (results_stats_dic['n_correct_breed'] /
+                                                         results_stats_dic['n_dogs_img'])
+    else:
+        results_stats_dic['pct_correct_breed'] = 0.0
+    
     # Calculates % correct dogs
     results_stats_dic['pct_correct_dogs'] = ((results_stats_dic['n_correct_dogs'] /
                                              results_stats_dic['n_dogs_img']) * 100.0)
